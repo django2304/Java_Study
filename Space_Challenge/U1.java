ec3f70bc5b04e68a11f7738a99867b714e525c31
Oleksandr Chichilinskyi <37484647+django2304@users.noreply.github.com>
2019-02-14 22:55:33 +0200

    Create U1.java

---
+++ Space_Challenge/U1.java
@@ -0,0 +1,38 @@
+public class U1 extend Rocket {
+  
+  private Random random = new Random();
+  private double chance = random.nextDouble();
+  private int rocketWeight = 10000;
+  private int allWeightWithCargo = rocketWeight + currentRocketWeight;
+  
+  public U1() {
+    weightLimit = 8000;
+    rocketCost = 1000000
+  }
+  
+ @Override
+    public boolean launch() {
+        //Chance of launch explosion = 5% * (cargo carried / cargo limit)
+        percent = 0.05;
+        double chanceOfExplosion = percent * ((double) allWeightWithCargo / (double) weightLimit);
+        if (chanceOfExplosion >= chance) {
+            System.out.println("U1 EXPLODED!");
+            return false;
+        } else
+            System.out.println("U1 launched successfully");
+        return true;
+    }
+
+  @Override
+    public boolean land() {
+        //Chance of landing crash = 1% * (cargo carried / cargo limit)
+        percent = 0.01;
+        double chanceOfCrash = percent * ((double) allWeightWithCargo / (double) weightLimit);
+        if (chanceOfCrash >= chance) {
+            System.out.println("U1 CRASHED!");
+            return false;
+        } else
+            System.out.println("U1 landed successfully");
+        return true;
+    }
+}
