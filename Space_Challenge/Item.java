f36e5e5584ae6ce02dbb9a8372d50bed6e2a2286
Oleksandr Chichilinskyi <37484647+django2304@users.noreply.github.com>
2019-02-14 22:34:31 +0200

    Create Item.java

---
+++ Space_Challenge/Item.java
@@ -0,0 +1,15 @@
+public class Item {
+  
+  private String name;
+  private int weight;
+  
+  public Item(String name, int weight) {
+    this.name = name;
+    this.weight = weight;
+  }
+  
+  @Override
+  public String toString() {
+    return name + weight;
+  }
+}
